b9263700bf53285f9d210ce4d51535edbe7c69a4
yigityalcin <yigityalciin@hotmail.com>
2023-04-18 14:21:29 +0300

    intro4

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const app = Vue.createApp({
     // data,functions
-    template: '<h2>I am the template</h2>'
+    // template: '<h2>I am the template</h2>'
+    data() { 
+        return {
+            title: 'The Final Empire',
+            author: 'Jack Harlow',
+            age: '45'
+        }
+    }
 })
 
 app.mount('#app')
