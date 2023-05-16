a448540f93a8af87fee337158932adaedde18c69
yigityalcin <yigityalciin@hotmail.com>
2023-05-17 01:10:57 +0300

    finish

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const app = Vue.createApp({
     // data,functions
     // template: '<h2>I am the template</h2>'
-    data() { 
+    data() {
         return {
+            showBooks: false,
             title: 'The Final Empire',
             author: 'Jack Harlow',
             age: '45'
         }
+    },
+    methods: {
+        changeTitle() {
+            this.title = "Donusum"
+
+        }
+
     }
 })
 
